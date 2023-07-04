cc05c5f6f71e58c15084366abad13cb559c0bf79
robertfeliciano <robertfeliciano314@gmail.com>
2023-07-04 14:59:07 -0400

    single vector

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+use std::ops::{Index, IndexMut};
 use std::{fmt::Display, fs};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Matrix {
     pub rows: usize,
     pub cols: usize,
-    pub data: Vec<Vec<f64>>,
+    pub data: Vec<f64>,
 }
 
 impl Matrix {
@@ -12,58 +13,78 @@ impl Matrix {
         Self {
             rows,
             cols,
-            data: vec![vec![0.0; cols]; rows],
+            data: vec![0.0; rows * cols],
         }
     }
 
     pub fn from_file(path: &str) -> Self {
         let content = fs::read_to_string(path).unwrap_or_else(|e| panic!("{e}"));
-        let mut matrix: Vec<Vec<f64>> = Vec::new();
-        for rows in content.lines() {
-            let mut row: Vec<f64> = Vec::new();
-            let entries: Vec<&str> = rows.split_whitespace().collect();
+        let mut data: Vec<f64> = Vec::new();
+        let mut cols: usize = 0;
+        let mut count: usize = 0;
 
-            entries
+        for r in content.lines() {
+            let entries: Vec<&str> = r.split_whitespace().collect();
+            let c = entries.len();
+            if count > 0 && cols != c {
+                panic!("Columns don't match");
+            }
+            cols = c;
+            count += 1;
+
+            let temp: Vec<f64> = entries
                 .iter()
-                .for_each(|ent| row.push(ent.parse::<f64>().unwrap()));
-
-            matrix.push(row);
+                .map(|ent| ent.parse::<f64>().unwrap())
+                .collect();
+            
+            for item in temp {
+                data.push(item);
+            }
         }
 
         Self {
-            rows: matrix.len(),
-            cols: matrix[0].len(),
-            data: matrix,
+            rows: content.lines().collect::<Vec<_>>().len(),
+            cols,
+            data,
         }
     }
 
     pub fn from_string(input: &str) -> Self {
-        let mut data: Vec<Vec<f64>> = Vec::new();
+        let mut data: Vec<f64> = Vec::new();
         let rows: Vec<&str> = input.split(';').collect();
-        for row in rows {
-            let entries: Vec<&str> = row.split_whitespace().collect();
-            let mut tmp_row: Vec<f64> = Vec::new();
+        let mut cols: usize = 0;
+        let mut count: usize = 0;
 
-            entries
+        for r in &rows {
+            let entries: Vec<&str> = r.split_whitespace().collect();
+            let c = entries.len();
+            if count > 0 && cols != c {
+                panic!("Columns don't match");
+            }
+            cols = c;
+            count += 1;
+
+            let temp: Vec<f64> = entries
                 .iter()
-                .for_each(|ent| tmp_row.push(ent.parse::<f64>().unwrap()));
-
-            data.push(tmp_row);
+                .map(|ent| ent.parse::<f64>().unwrap())
+                .collect();
+            
+            for item in temp {
+                data.push(item);
+            }
         }
 
-        let n_r = data.len();
-        let n_c = data[0].len();
         Self {
-            rows: n_r,
-            cols: n_c,
+            rows: rows.len(),
+            cols,
             data,
         }
     }
 
     pub fn copy(&self) -> Self {
-        let mut n_data: Vec<Vec<f64>> = Vec::new();
+        let mut n_data: Vec<f64> = Vec::new();
 
-        self.data.iter().for_each(|row| n_data.push(row.to_vec()));
+        self.data.iter().for_each(|elem| n_data.push(*elem));
 
         Self {
             rows: self.rows,
@@ -71,10 +92,15 @@ impl Matrix {
             data: n_data,
         }
     }
-    
+
     pub fn print(&self) {
-        self.data.iter().for_each(|v| println!("{:?}", v));
-        println!();
+        for r in 0..self.rows {
+            print!("[");
+            for c in 0..self.cols {
+                if c == self.cols - 1 { print!("{:.3}", self[r][c]); } else { print!("{:.3} ", self[r][c]); }
+            }
+            println!("]");
+        }
     }
 
     pub fn identity(&mut self) {
@@ -82,30 +108,24 @@ impl Matrix {
             panic!("Not a square matrix.");
         }
         for r in 0..self.rows {
-            self.data[r][r] = 1.0;
+            self[r][r] = 1.0;
         }
     }
 
     pub fn apply(&mut self, f: impl Fn(f64) -> f64) {
-        self.data = self
-            .data
-            .iter()
-            .map(|v| v.iter().map(|x| f(*x)).collect())
-            .collect();
+        self.data = self.data.iter().map(|elem| f(*elem)).collect()
     }
 
     pub fn combine(&self, b: Self, f: impl Fn(f64, f64) -> f64) -> Self {
         if self.rows != b.rows || self.cols != b.cols {
-            panic!("Matrices must be of the same size");
+            panic!("Matrices must be of the same size.");
         }
         let mut new_matrix = Self::new(self.rows, self.cols);
-        for r in 0..self.rows {
-            new_matrix.data[r] = self.data[r]
-                .iter()
-                .zip(b.data[r].iter())
-                .map(|(a, b)| f(*a, *b))
-                .collect();
-        }
+        new_matrix.data = self.data
+            .iter()
+            .zip(b.data.iter())
+            .map(|(a, b)| f(*a, *b))
+            .collect();
         new_matrix
     }
 
@@ -121,30 +141,33 @@ impl Matrix {
             for j in 0..b.cols {
                 let mut sum = 0.0;
                 for k in 0..b.rows {
-                    sum += self.data[i][k] * b.data[k][j];
+                    sum += self[i][k] * b[k][j];
                 }
-                dp.data[i][j] = sum;
+                dp[i][j] = sum;
             }
         }
         dp
     }
 
     pub fn rref(&mut self) {
-        if self.data[0][0] == 0.0 {
+        if self[0][0] == 0.0 {
             self.swap_rows(0);
         }
         let mut lead: usize = 0;
         let rows = self.rows;
         while lead < rows {
             for r in 0..rows {
-                let div = self.data[lead][lead];
-                let mult = self.data[r][lead] / div;
+                let div = self[lead][lead];
+                let mult = self[r][lead] / div;
 
                 if r == lead {
-                    self.data[lead] = self.data[lead].iter().map(|entry| entry / div).collect();
+                    // self[lead] = self[lead].iter().map(|entry| entry / div).collect::<Vec<_>>();
+                    self[lead]
+                        .iter_mut()
+                        .for_each(|elem| *elem = (*elem) / div);
                 } else {
                     for c in 0..self.cols {
-                        self.data[r][c] -= self.data[lead][c] * mult;
+                        self[r][c] -= self[lead][c] * mult;
                     }
                 }
             }
@@ -164,16 +187,17 @@ impl Matrix {
                 if c == j {
                     continue;
                 }
-                v.push(self.data[r][c]);
+                v.push(self[r][c]);
             }
             cut.push(v);
         }
+        let flattened = cut.clone().into_iter().flatten().collect();
         let n_r = cut.len();
         let n_c = cut[0].len();
         let minor = Self {
             rows: n_r,
             cols: n_c,
-            data: cut,
+            data: flattened,
         }
         .det();
         let base: i32 = -1;
@@ -188,13 +212,13 @@ impl Matrix {
             );
         }
         if self.rows == 2 && self.cols == 2 {
-            self.data[0][0] * self.data[1][1] - self.data[0][1] * self.data[1][0]
+            self[0][0] * self[1][1] - self[0][1] * self[1][0]
         } else {
             let row: usize = 1;
             let mut det = 0.0;
 
-            for j in 0..self.data[row].len() {
-                det += self.cofactor(row, j) * self.data[row][j];
+            for j in 0..self[row].len() {
+                det += self.cofactor(row, j) * self[row][j];
             }
             det
         }
@@ -204,8 +228,22 @@ impl Matrix {
         let mut t = Self::new(self.cols, self.rows);
         for i in 0..self.rows {
             for j in 0..self.cols {
-                t.data[j][i] = self.data[i][j];
+                t[j][i] = self[i][j];
             }
+        }
+        t
+    }
+
+    pub fn trace(&self) -> f64 {
+        if self.rows != self.cols {
+            panic!(
+                "Trace requires matrix to be square. Input matrix was {:?}.",
+                self
+            );
+        }
+        let mut t: f64 = 0.0;
+        for i in 0..self.rows {
+            t += self[i][i];
         }
         t
     }
@@ -220,7 +258,7 @@ impl Matrix {
 
         for row in 0..self.rows {
             for col in 0..self.cols {
-                inv.data[row][col] = self.cofactor(row, col);
+                inv[row][col] = self.cofactor(row, col);
             }
         }
 
@@ -233,32 +271,34 @@ impl Matrix {
     fn swap_rows(&mut self, row: usize) {
         let mut n_r = 0;
         for r in 0..self.rows {
-            if self.data[r][0] > 0.0 {
+            if self[r][0] > 0.0 {
                 n_r = r;
                 break;
             }
         }
-        let temp: Vec<f64> = self.data[row].clone();
-        self.data[row] = self.data[n_r].clone();
-        self.data[n_r] = temp;
+        let temp: Vec<f64> = self[row].to_vec();
+        for c in 0..self.cols {
+            self[row][c] = self[n_r][c];
+            self[n_r][c] = temp[n_r * self.cols + c];
+        }
     }
 
     fn correct(&mut self) {
         for row in 0..self.rows {
             for col in 0..self.cols {
-                let elem = self.data[row][col];
+                let elem = self[row][col];
                 if elem == -0.0 {
-                    self.data[row][col] = 0.0;
+                    self[row][col] = 0.0;
                 }
                 let floored = elem.floor();
                 if elem - floored > 0.9999999 {
-                    self.data[row][col] = elem.round();
+                    self[row][col] = elem.round();
                 }
                 if elem > 0.0 && elem < 0.000001 {
-                    self.data[row][col] = 0.0;
+                    self[row][col] = 0.0;
                 }
                 if elem < 0.0 && elem > -0.00001 {
-                    self.data[row][col] = 0.0;
+                    self[row][col] = 0.0;
                 }
             }
         }
@@ -267,11 +307,30 @@ impl Matrix {
 
 impl Display for Matrix {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for v in self.data.iter() {
-            writeln!(f, "{:?}", v)?;
+        for r in 0..self.rows {
+            write!(f, "[")?;
+            for c in 0..self.cols {
+                if c == self.cols - 1 { write!(f, "{:.3}", self[r][c])?; } else { write!(f, "{:.3} ", self[r][c])?; }
+                
+            }
+            writeln!(f, "]")?;
         }
 
         Ok(())
+    }
+}
+
+impl Index<usize> for Matrix {
+    type Output = [f64];
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.data[index * self.cols..(index + 1) * self.cols]
+    }
+}
+
+impl IndexMut<usize> for Matrix {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.data[index * self.cols..(index + 1) * self.cols]
     }
 }
 
@@ -285,7 +344,7 @@ mod tests {
         let expected = Matrix {
             rows: 2,
             cols: 3,
-            data: vec![vec![1.0, 2.0, 3.0], vec![4.0, 5.0, 6.0]],
+            data: vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
         };
 
         assert!(m == expected);
@@ -294,7 +353,7 @@ mod tests {
     #[test]
     fn test_display() {
         let m = Matrix::from_string("1 2 3 ; 4 5 6");
-        
+
         assert_eq!("[1.0, 2.0, 3.0]\n[4.0, 5.0, 6.0]\n", m.to_string())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use linalg::Matrix;
 
 fn main() {
-    let m1 = Matrix::from_string("1 2 3 ; 4 5 6");
-    println!("{m1}");
-    let m2 = Matrix::from_string("7 8 9; 10 11 12");
-    println!("{m2}");
-    let m3 = m1.combine(m2, |a, b| (a * b));
-    println!("{m3}");
+    // let m1 = Matrix::from_string("1 2 3 ; 4 5 6");
+    // println!("{m1}");
+    // let m2 = Matrix::from_string("7 8 9; 10 11 12");
+    // println!("{m2}");
+    // let m3 = m1.combine(m2, |a, b| (a * b));
+    // println!("{m3}");
 
     // let mut m: Matrix = Matrix::new(3,3);
     // m.identity();
 
     // let mut mcpy = m.copy();
     // mcpy.apply(|x| x+3.0);
-    // mcpy.print();
+    // println!("{mcpy}");
 
-    // m.print();
+    // println!("{m}");
 
     // let m1 = Matrix::from_file("src/2b2.txt");
     // m1.print();
@@ -52,19 +52,18 @@ fn main() {
     // m5i.print();
 
     // // let mut m6 = Matrix::from_string("0 1 2 ; 0 3 1 ; 5 2 2");
-    // let mut m6 = Matrix::from_string("5 -6 -7 7 ;
-    //                                                 3 -2 5 -17 ;
-    //                                                 2 4 -3 29");
-    // m6.print();
-    // println!("Row Reduce Echelon Form calculation:");
-    // // println!("{:?}", m6);
-    // m6.rref();
-    // m6.print();
+    let mut m6 = Matrix::from_string("5 -6 -7 7 ;
+                                                    3 -2 5 -17 ;
+                                                    2 4 -3 29");
+    m6.print();
+    println!("Row Reduce Echelon Form calculation:");
+    m6.rref();
+    m6.print();
 
-    // let mut m7 = Matrix::from_file("src/4b5.txt");
-    // m7.print();
-    // println!("{:?}", m7);
-    // println!("Row Reduce Echelon Form calculation:");
-    // m7.rref();
-    // m7.print();
+    let mut m7 = Matrix::from_file("src/4b5.txt");
+    m7.print();
+    println!("{:?}", m7);
+    println!("Row Reduce Echelon Form calculation:");
+    m7.rref();
+    m7.print();
 }
